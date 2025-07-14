f37305fa54293af020aceec2f21bcc47fe3833d6
Joel <joeldlossantos03@gmail.com>
2025-07-13 23:45:05 -0400

    Actualización

---
+++ Tarea 2/Programas/Ejercicios 2.1 al 2.16/Ejercicio 2.12/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+/* Igualdad de expresiones.
+El programa, al recibir como daros T, P y N,comprueba
+la igualdad de una expreción determinada.
+
+T, P y N: variables de tipo entero. */
+
+void main (void)
+{
+    int T, P, N;
+    printf("Ingre los valores de T, P y N:");
+    scanf ("%d %d %d" , &T, &P, &N);
+    if (P !=0)
+    {
+        if (pow (T / P, N)== (pow/(T,N))/ pow (P, N))
+        else
+        printf ("\nNo se comprueba la igualdas")
+    }
+}
