1b88d9e601d5bad54cafa5d15c2f1bb56bf93b13
Joel <joeldlossantos03@gmail.com>
2025-07-14 00:17:31 -0400

    actualización

---
+++ Tarea 2/Programas/Ejercicios 2.1 al 2.16/Convertir a if 2.5/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+/* Función matemática.
+El programa obtiene el resultado de una función.
+OP y T: variable de tipo entero.
+RES: variable de tipo real. */
+
+void main(void)
+
+{
+int OP, T;
+float RES;
+printf("Ingrese la opción del cálculo y el valor entero:");
+scanf("%d %d", &OP, &T);
+if (OP == 1 )
+    RES = T / 5 ;
+else if (OP == 2)
+    RES = pow (T , T);
+else if (OP == 3 || OP == 4 )
+    RES = 6 * T/2;
+else
+    RES = 1;
+
+    printf ("\nResultado: %7.2f" ,RES);
+}
